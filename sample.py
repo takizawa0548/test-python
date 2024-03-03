7a2d12d289b28cba0b4abf7961ee36d535373297
mr_noppo <takizawa20383461@gmail.com>
2024-03-04 06:21:38 +0900

    fc

---
+++ sample.py
@@ -0,0 +1,10 @@
+class MyInt:
+    def __init__(self, val)
+        self.value = val
+    def __add__(self, other):
+        return MyInt(self.value + other)
+    def add(x,y):
+        """加算
+        add(2,4)は6を返す
+        """
+        return x + y #ex:2+4=6
